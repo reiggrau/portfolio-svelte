56c671d8884b13ad5efe3ca6e9c06c93b7b53d14
guillemreig <guillemreiggrau@gmail.com>
2023-10-31 20:42:12 +0100

    Planning layout

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,9 @@
+<script lang='ts'>
+
+</script>
+
+<h1>Navbar</h1>
+
+<style lang='postcss'>
+    
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang='ts'>
+    import Navbar from '$lib/components/Navbar.svelte';
+</script>
+
+<div id='home-layout'>
+    <Navbar/>
+    <slot/>
+</div>
